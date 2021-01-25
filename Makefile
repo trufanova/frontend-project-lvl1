7d5b6239cc3547586a2cc3695551b902e9d85294
Maria Trufanova <5014685@mail.ru>
2021-01-25 15:35:23 +0300

    add game brain-gcd, add to makefile command link and brain-gcd, get brain-gcd to package.json

--- Makefile
+++ Makefile
@@ -4,8 +4,12 @@ brain-games:
 	node bin/brain-games.js
 brain-even:
 	node bin/brain-even.js
+brain-gcd:
+	node bin/brain-gcd.js
 publish:
 	sudo npm publish --dry-run
 lint: 
 	npx eslint . --fix
+link:
+	sudo npm link
 	
---
+++ bin/brain-gcd.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import hello from '../src/sli.js';
+import {
+  getRandom, getCongratulations, wrongAnswerMessage, correctAnswerMessage, askThreeQuestions,
+} from '../index.js';
+
+let random1 = 0;
+let random2 = 0;
+let counter = 0;
+
+const getCorrectAnswer = (num1, num2) => {
+  if (num2) {
+    return getCorrectAnswer(num2, num1 % num2);
+  }
+  return Math.abs(num1);
+};
+
+const gcd = () => {
+  console.log('Find the greatest common divisor of given numbers.');
+  const question = () => {
+    random1 = getRandom(1, 100);
+    random2 = getRandom(1, 100);
+    const correctAnswer = getCorrectAnswer(random1, random2);
+
+    console.log(`Question: ${random1} ${random2}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (Number(correctAnswer) === Number(answer)) {
+      correctAnswerMessage();
+      counter += 1;
+    } else {
+      wrongAnswerMessage(answer, correctAnswer);
+    }
+  };
+  askThreeQuestions(question);
+  getCongratulations(counter);
+};
+
+hello();
+gcd();
