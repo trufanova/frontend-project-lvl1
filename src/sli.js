d73bc223c09eff1a5604f6a63201e075dfa1ecd8
Maria Trufanova <5014685@mail.ru>
2021-01-25 21:17:56 +0300

    fix error

--- src/sli.js
+++ src/sli.js
@@ -1,13 +1,13 @@
 import readlineSync from 'readline-sync';
 
 let gettedName = '';
+const getNameUser = () => gettedName;
 
 const hello = () => {
   console.log('Welcome to the Brain Games!');
   gettedName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${getNameUser()}`);
 };
-const getNameUser = () => gettedName;
 
 export default hello;
 export { getNameUser };
